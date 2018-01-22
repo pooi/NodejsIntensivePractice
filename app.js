6da350afcce80b682a6f9c1b609243da416c2634
pooi <ldayou@naver.com>
2018-01-22 11:28:12 +0900

    Add feature   * Added 404 error page.

--- app.js
+++ app.js
@@ -33,10 +33,29 @@ app.use('/find', find);
 app.use('/lost', lost);
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+// app.use(function (req, res, next) {
+//     var err = new Error('Not Found');
+//     err.status = 404;
+//     next(err);
+// });
+
+app.use(function(req, res, next){
+    res.status(404);
+
+    // respond with html page
+    if (req.accepts('html')) {
+        res.render('404', { url: req.url });
+        return;
+    }
+
+    // respond with json
+    if (req.accepts('json')) {
+        res.send({ error: 'Not found' });
+        return;
+    }
+
+    // default to plain-text. send()
+    res.type('txt').send('Not found');
 });
 
 // error handler
