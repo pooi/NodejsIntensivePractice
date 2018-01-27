1a770d0fab5e72132a26287d2318eae13f9a674a
pooi <ldayou@naver.com>
2018-01-27 12:48:26 +0900

    Change index page   * Changed index page that adapted vue.js

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
+router.get('/pre', function (req, res, next) {
     res.render('index');
+});
+
+router.get('/', function (req, res, next) {
+    res.render('vue_index');
 });
 
 module.exports = router;
